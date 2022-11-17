5ba3fc5cfd96a68e970104cae55a91e163597cb0
FG0001 <fgiessow@gmail.com>
2022-11-17 01:33:39 +0000

    11162022

--- dokuwiki/main.tf
+++ dokuwiki/main.tf
@@ -47,6 +47,15 @@ resource "google_compute_firewall" "default-firewall" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+resource "google_compute_disk" "dockuwiki" {
+  name  = "dockuwiki"
+  type  = "pd-ssd"
+  labels = {
+    environment = "dev"
+  }
+  size = "16"
+}
+
 output "external-ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
